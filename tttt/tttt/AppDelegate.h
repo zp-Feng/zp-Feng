df705b1f8b551c37b11d1bb3ade957d4d11133e3
zpfeng <ZpFeng@zp-feng-macbook.local>
2015-12-29 15:28:15 +0800

    yyy

---
+++ tttt/tttt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tttt
+//
+//  Created by 丰志鹏 on 15/12/29.
+//  Copyright © 2015年 ZpFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tttt/tttt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tttt
+//
+//  Created by 丰志鹏 on 15/12/29.
+//  Copyright © 2015年 ZpFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
